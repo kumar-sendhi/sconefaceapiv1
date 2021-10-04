ac5d3669926ccd461d5a24a31b178ad0a1b3df07
Gowtham Kumar <36653915+gowtham9394@users.noreply.github.com>
2021-10-04 11:40:38 +0000

    Update app.py

--- hello-python/app/app.py
+++ hello-python/app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, jsonify, request, redirect
 from flask.wrappers import Response
 import person_group_operations,person_operations,person_face_operations
 import uuid
+import ssl
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -132,4 +133,6 @@ def person_identify():
 
 if __name__ == "__main__":
     #app.run(host="0.0.0.0",port = 8000,debug=True)
-    app.run(host="0.0.0.0")
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.load_cert_chain("/app/cert.pem", "/app/key.pem")
+    app.run(host="0.0.0.0",ssl_context=context)
